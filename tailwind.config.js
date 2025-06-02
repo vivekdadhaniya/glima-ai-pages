6bf81315f27add8748b07b33232aabafab443d8f
harsh987654321 <vivek.dadhaniya@gmail.com>
2025-06-02 17:21:07 +0530

    image modal & video modals pages added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*",
-    "./style_lora_single_pages/**/*.html",   
-    "./Image_Ai_Tool_Single_page/**/*.html",   
-    "./Image_style_menual_single_page/**/*.html",   
-    "./pikka_Ai_pages/**/*.html", 
-    "./Video_template/**/*.html",  
-    "./video_Generator/**/*.html",  
-    "./nsfw/**/*.html",  
-    "./open-art/**/*.html",  
-    
+    "./style_lora_single_pages/**/*.html",
+    "./Image_Ai_Tool_Single_page/**/*.html",
+    "./Image_style_menual_single_page/**/*.html",
+    "./pikka_Ai_pages/**/*.html",
+    "./Video_template/**/*.html",
+    "./video_Generator/**/*.html",
+    "./nsfw/**/*.html",
+    "./open-art/**/*.html",
+    "./Image-models-template/**/*.html",
+    "./Video-models-template/**/*.html",
+    "./Imagine-art-image/**/*.html",
   ],
   theme: {
     extend: {
@@ -21,7 +23,7 @@ module.exports = {
         'limegreen': '#C6FF00',
         'black-0a0a0a': '#0A0A0A',
         'black-1B1B1B': '#1B1B1B',
-        'gray-B1B1B1':'#B1B1B1',
+        'gray-B1B1B1': '#B1B1B1',
       },
       scrollbar: {
         hide: {
